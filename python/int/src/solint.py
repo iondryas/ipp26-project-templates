81c385ac893f00c9170f99d52dede9cceaad8d30
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2026-02-26 10:48:34 +0100

    feat(py,int): add logging support

--- python/int/src/solint.py
+++ python/int/src/solint.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+import logging
+import sys
 from io import StringIO
 from pathlib import Path
 
@@ -9,6 +11,23 @@ from interpreter.interpreter import Interpreter
 
 
 def main() -> None:
+    """
+    The main entry point for the SOL26 interpreter. It parses command-line arguments, and uses
+    the Interpreter class to load and execute the specified program in the SOL-XML format.
+
+    IPP: Do not modify this function, except for adding additional CLI arguments if you wish.
+    """
+
+    # Set up logging
+    # IPP: You do not have to use logging – but it is the recommended practice.
+    # See this for more information: https://docs.python.org/3/howto/logging.html
+    logging.basicConfig(
+        stream=sys.stderr,
+        level=logging.WARNING,
+        format="%(asctime)s %(levelname)s [%(name)s][%(filename)s:%(lineno)d] %(message)s",
+    )
+    logger = logging.getLogger("main")
+
     # Define the CLI arguments
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
@@ -25,6 +44,13 @@ def main() -> None:
         required=False,
         help="Path to a file that will be used as the standard input "
         "for the interpreted program (optional).",
+    )
+    arg_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Enable verbose logging output (using once = INFO level, using twice = DEBUG level).",
     )
 
     # Parse the provided arguments
@@ -45,12 +71,19 @@ def main() -> None:
     if input_file is not None and not input_file.is_file():
         ErrorCode.GENERAL_INPUT.fire("Input file does not exist or is not a file.")
 
+    # Enable debug or info logging if the verbose flag was set twice or once
+    if args.verbose >= 2:
+        logging.root.setLevel(logging.DEBUG)
+    elif args.verbose == 1:
+        logging.root.setLevel(logging.INFO)
+
     # Create an instance of the interpreter
     interpreter = Interpreter()
-    # Load the program from the source file
-    interpreter.load_program(source_file)
 
     try:
+        # Load the program from the source file
+        interpreter.load_program(source_file)
+
         if input_file is not None:
             # Execute the program using the provided input file as standard input
             with input_file.open() as input_io:
@@ -59,13 +92,16 @@ def main() -> None:
             # Execute the program with an empty input stream if no input file was provided
             interpreter.execute(StringIO())
     except InterpreterError as e:
+        logger.debug("InterpreterError", exc_info=e)
         e.error_code.fire(str(e))
-    except SystemExit:
+    except SystemExit as e:
         # You are NOT allowed to use exit(), sys.exit(), etc. anywhere in your code.
         # Handle interpretation errors by raising an appropriate InterpreterError
         # with the correct error code.
+        logger.error("Caught disallowed SystemExit with code %s", e.code)
         exit(120)
-    except Exception as e:  # noqa: BLE001
+    except Exception as e:
+        logger.error("Unhandled exception during interpretation", exc_info=e)
         ErrorCode.GENERAL_OTHER.fire(str(e))
 
 
